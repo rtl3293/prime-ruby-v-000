5c5d24128e8f5f20a1a730eeccc67a35e4324e57
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 19:25:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,16 @@
 # Add  code here!
 def prime?(number)
-  begin_range = number/-2 - 1
-  end_range = number/2 + 1
-  factors = ((begin_range)..(end_range)).to_a
   if number < 0
     number = (number * -1)
+    begin_range = number/2 -1
+    end_range = number/-2 + 1
+  else
+    begin_range = number/-2 - 1
+    end_range = number/2 + 1
   end
+
+  factors = ((begin_range)..(end_range)).to_a
+
   if number % 2 == 0
     return false
   else
