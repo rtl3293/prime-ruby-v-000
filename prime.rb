64c1ff20647ed9611fede51c9c17f82f425d6769
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 19:46:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,15 +8,15 @@ def prime?(number)
     begin_range = 1
     end_range = number/2 + 1
     factors = ((begin_range)..(end_range)).to_a
-    
+
     if number % 2 == 0
       return false
     else
       prime = factors.any? do |factor|
         number % factor == 0
       end
-      return prime
       binding.pry
+      return prime
     end
   end
 end
