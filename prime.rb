139f9c03e103d91b764bb9ebb683442cb12a0568
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 19:36:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,17 +7,17 @@ def prime?(number)
   elsif number >= -1 || number <= 1
     return true
   else
-  begin_range = 1
-  end_range = number/2 + 1
-  factors = ((begin_range)..(end_range)).to_a
+    begin_range = 1
+    end_range = number/2 + 1
+    factors = ((begin_range)..(end_range)).to_a
 
-  if number % 2 == 0
-    return false
-  else
-    prime = factors.any? do |factor|
-      number % factor == 0
+    if number % 2 == 0
+      return false
+    else
+      prime = factors.any? do |factor|
+        number % factor == 0
+      end
+      return prime
     end
-    return prime
-  end
   end
 end
